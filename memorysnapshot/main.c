c0d85664dce2606deb18d2a91606b1c04568be5a
JadenLeung <j28leung@uwaterloo.ca>
2025-02-26 10:40:00 -0500

    created main.c

--- memorysnapshot/main.c
+++ memorysnapshot/main.c
@@ -5,23 +5,43 @@
 
 struct player {
     char *name;
-    int points;
     int fg2;
     int fg3;
 };
 
-int compareval = 0;
+const int POSSESSIONS = 20;
 
-const struct player BRONNY = {"Bronny", 18, 37, 21};
+struct player BRONNY = {"Bronny", 37, 21};
 
-
-int one_v_one(struct player *a, struct player *b) {
-    printf("%s vs %s", a->name, b->name);
+void one_v_one(struct player *a, struct player *b) {
+    printf("%s vs %s\n", a->name, b->name);
+    if (strcmp(a->name, b->name) > 0) {
+        b->name[0] = 'J';
+    } else {
+        b->name += 2;
+    }
+    // SNAPSHOT A
+    printf("%s vs %s\n", a->name, b->name);
+    a->name++;
+    char temp[7] = {0};
+    strcpy(temp, a->name);
+    temp[0] = 'S';
+    temp[3] = '\0';
+    printf("%s vs %s\n", temp, b->name);
+    // SNAPSHOT B
+    strcpy(b->name, temp);
+    if (a->fg2 * 2 < b->fg3 * 3) {
+        b->name[1] = 'u';
+    } else {
+        temp[1] = 'u';
+    }
+    // SNAPSHOT C
+    printf("%s vs %s\n", temp, b->name);
 }
 
 int main(void) {
-    char name[7] = "LeBron";
+    char name[8] = "LeBron";
     char *storename = name;
-    struct player LEBRON = {name, 25, 25, 40};
-    one_v_one(&LEBRON, &BRONNY);
+    struct player LEBRON = {storename, 25, 40};
+    one_v_one(&BRONNY, &LEBRON);
 }
